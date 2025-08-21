fc59de994de64f4748a9254fdc2d6eea5c89c255
João Carlos Viana Teixeira <joaocvteixeira@outlook.com>
2025-08-20 22:33:59 -0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+    String nomes[] = {"Samara", "Joana", "Helóisa", "João"};
+    System.out.println(nomes[3]);
+    }
+}
